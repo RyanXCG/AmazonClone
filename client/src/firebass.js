2979543c326552b67597f7fedb3aee175477f3da
RyanXCG <xiayu1994129@gmail.com>
2021-01-07 15:40:27 -0500

    main page and react contextAPI finished

---
+++ client/src/firebass.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCTADlFlRmlXvsKlxtWj1mpsp78GwbFIjY",
+  authDomain: "clone-427c3.firebaseapp.com",
+  projectId: "clone-427c3",
+  storageBucket: "clone-427c3.appspot.com",
+  messagingSenderId: "356907050512",
+  appId: "1:356907050512:web:4f63c8382c8913528d8c95",
+  measurementId: "G-5KVS3BYLXB",
+};
