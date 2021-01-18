28c3b3b0d6eb26a49bbfc89fbec2ce8cd9a28d65
RyanXCG <xiayu1994129@gmail.com>
2021-01-18 01:07:05 -0500

    delopyed the base functionality build, Yay

--- client/functions/index.js
+++ client/functions/index.js
@@ -15,7 +15,7 @@ app.use(cors({ origin: true }));
 app.use(express.json());
 
 // API routes
-//app.get("/", (req, res) => res.status(200).send("hello world"));
+app.get("/", (req, res) => res.status(200).send("hello world"));
 
 app.post("/payments/create", async (req, res) => {
   const total = req.query.total;
